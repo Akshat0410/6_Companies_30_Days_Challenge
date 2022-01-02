14b785571248ad78f62b2130a7278104a3cfb026
Akshat0410 <anupamp74@gmail.com>
2022-01-02 14:10:47 +0530

    Question RunLengthEncoding solved

---
+++ src/com/anupam/challenge/Day2/RunLengthEncoding.java
@@ -0,0 +1,32 @@
+package com.anupam.challenge.Day2;
+import java.util.Scanner;
+
+public class RunLengthEncoding {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        String ans=encode(s);
+        System.out.println(ans);
+    }
+
+    private static String encode(String s) {
+
+        int count = 1, i;
+        StringBuilder ans = new StringBuilder();
+        for (i = 0; i < s.length() - 1; i++) {
+            char c = s.charAt(i);
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                count++;
+            } else {
+                ans.append(c).append(count);
+                count = 1;
+            }
+
+        }
+
+        ans.append(s.charAt(i)).append(count);
+        return ans.toString();
+
+    }
+}

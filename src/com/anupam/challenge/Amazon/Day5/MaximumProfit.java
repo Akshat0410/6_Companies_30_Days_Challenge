b97786ff9cbab58743eb9f099fa2a07ea33cbaf0
Akshat0410 <anupamp74@gmail.com>
2022-01-06 15:17:24 +0530

    Question Maximum Profit Solved

---
+++ src/com/anupam/challenge/Amazon/Day5/MaximumProfit.java
@@ -0,0 +1,42 @@
+package com.anupam.challenge.Amazon.Day5;
+
+//Question :- Maximum Profit
+
+import java.util.Scanner;
+
+public class MaximumProfit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), k = sc.nextInt();
+        int[] price = new int[n];
+        for (int i = 0; i < n; i++) {
+            price[i] = sc.nextInt();
+        }
+
+        int profit = maxProfit(k, n, price);
+        System.out.println(profit);
+    }
+
+    private static int maxProfit(int K, int N, int[] A) {
+
+        int[][] dp = new int[K + 1][N];
+        for (int t = 1; t <= K; t++) {
+            int max = Integer.MIN_VALUE;
+
+            for (int d = 1; d < A.length; d++) {
+                if (dp[t - 1][d - 1] - A[d - 1] > max) {
+                    max = dp[t - 1][d - 1] - A[d - 1];
+                }
+
+                if (max + A[d] > dp[t][d - 1]) {
+                    dp[t][d] = max + A[d];
+                } else {
+                    dp[t][d] = dp[t][d - 1];
+                }
+            }
+        }
+
+        return dp[K][N - 1];
+
+    }
+}

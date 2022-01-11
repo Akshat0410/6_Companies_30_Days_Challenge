22565cb8184b215667b14fb61795cad115d8a8a5
Akshat0410 <anupamp74@gmail.com>
2022-01-11 16:43:14 +0530

    Last Question Solved

--- src/com/anupam/challenge/Amazon/Day10/DeleteNafterMnodes.java
+++ src/com/anupam/challenge/Amazon/Day10/DeleteNafterMnodes.java
@@ -1,4 +1,7 @@
 package com.anupam.challenge.Amazon.Day10;
+
+//Question :- Delete N nodes after M nodes of a linked list
+
 public class DeleteNafterMnodes {
 
     private static void linkDelete(Node head, int m, int n) {

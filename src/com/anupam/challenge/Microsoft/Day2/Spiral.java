956ee420235559f1d2853821340239c55f6fe046
Akshat0410 <anupamp74@gmail.com>
2022-01-13 10:56:08 +0530

    Question Solved Today

---
+++ src/com/anupam/challenge/Microsoft/Day2/Spiral.java
@@ -0,0 +1,57 @@
+package com.anupam.challenge.Microsoft.Day2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Spiral {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt(),c=sc.nextInt();
+        int[][] ar=new int[r][c];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                ar[i][j]=sc.nextInt();
+            }
+        }
+
+        ArrayList<Integer> ans=spirallyTraverse(ar,r,c);
+        System.out.println(ans);
+    }
+
+    private static ArrayList<Integer> spirallyTraverse(int[][] matrix, int r, int c) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        int m = 0;
+        int n = 0;
+        int count = 0;
+        for(int i = 0 ; i < r*c ; i ++)
+        {
+            arr.add(matrix[m][n]);
+            if((m == count) && (n < (c - count - 1)))
+            {
+                n++;
+            }
+            else if((n == c - 1 - count) && (m < (r - count - 1)))
+            {
+                m++;
+            }
+            else if((m == r - 1 - count) && (n > count))
+            {
+                n--;
+            }
+            else if((n == count) && (m > count))
+            {
+                m--;
+                if(m == count)
+                {
+                    count++;
+                    m = count;
+                    n = count;
+                }
+            }
+        }
+        return arr;
+    }
+
+
+}
---
+++ src/com/anupam/challenge/Microsoft/Day2/SpiralMatrix.java
@@ -0,0 +1,67 @@
+package com.anupam.challenge.Microsoft.Day2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SpiralMatrix {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt(),c=sc.nextInt();
+        int[][] ar=new int[r][c];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                ar[i][j]=sc.nextInt();
+            }
+        }
+
+        ArrayList<Integer> ans=spirallyTraverse(ar,r,c);
+        System.out.println(ans);
+    }
+
+    private static ArrayList<Integer> spirallyTraverse(int[][] matrix, int r, int c) {
+
+        ArrayList<Integer> ans=new ArrayList<>();
+        int minr=0,minc=0,maxr=matrix.length,maxc=matrix[0].length,cnt=0,tno=r*c;
+
+        while(cnt<tno){
+
+            //top wall
+
+            for(int i=minc;i<maxc && cnt<tno;i++){
+              ans.add(matrix[minr][i]);
+              cnt++;
+            }
+            minr++;
+
+            //right wall
+
+            for(int i=maxc,j=minr;j<maxr && cnt<tno;j++){
+                ans.add(matrix[j][i]);
+                cnt++;
+            }
+            maxc--;
+
+            //bottom wall
+
+            for(int j=maxr,i=maxc;i>=minc && cnt<tno;i--){
+                ans.add(matrix[j][i]);
+                cnt++;
+            }
+
+            maxr--;
+
+
+            //left wall
+
+            for(int j=maxr,i=minc;j>=minr && cnt<tno;j--){
+                ans.add(matrix[j][i]);
+                cnt++;
+            }
+
+            minc++;
+        }
+
+        return ans;
+    }
+}

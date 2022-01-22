14445bb870d603d2f26437688599181950e98db0
Akshat0410 <anupamp74@gmail.com>
2022-01-22 21:07:37 +0530

    Question 2 solved

---
+++ src/com/anupam/challenge/Intuit/Day21/WordSearch.java
@@ -0,0 +1,46 @@
+package com.anupam.challenge.Intuit.Day21;
+
+//Question :- Word Search
+
+import java.util.Scanner;
+
+public class WordSearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        char[][] board = new char[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                board[i][j] = sc.next().charAt(0);
+            }
+        }
+        String st = sc.next();
+        boolean ans = isWordExist(board, st);
+        System.out.println(ans);
+    }
+
+
+    private static boolean isWordExist(char[][] board, String word) {
+        char[] w = word.toCharArray();
+        for (int y = 0; y < board.length; y++) {
+            for (int x = 0; x < board[y].length; x++) {
+                if (exist(board, y, x, w, 0)) return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean exist(char[][] board, int y, int x, char[] word, int i) {
+        if (i == word.length) return true;
+        if (y < 0 || x < 0 || y == board.length || x == board[y].length) return false;
+        if (board[y][x] != word[i]) return false;
+        board[y][x] ^= 256;
+        boolean exist = exist(board, y, x + 1, word, i + 1)
+                || exist(board, y, x - 1, word, i + 1)
+                || exist(board, y + 1, x, word, i + 1)
+                || exist(board, y - 1, x, word, i + 1);
+        board[y][x] ^= 256;
+        return exist;
+    }
+}

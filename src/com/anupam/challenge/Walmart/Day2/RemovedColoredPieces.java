de7c2f9ba01af79046a20ba2d471bdd162403368
Akshat0410 <anupamp74@gmail.com>
2022-01-28 21:21:08 +0530

    3rd question solved

---
+++ src/com/anupam/challenge/Walmart/Day2/RemovedColoredPieces.java
@@ -0,0 +1,35 @@
+package com.anupam.challenge.Walmart.Day2;
+
+//Question :-  Remove Colored Pieces if Both Neighbors are the Same Color
+
+import java.util.Scanner;
+
+public class RemovedColoredPieces {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String color = sc.next();
+        boolean ans = winnerOfGame(color);
+        System.out.println(ans);
+    }
+
+    private static boolean winnerOfGame(String colors) {
+        char prev = 'C';
+        int a = 0, b = 0;
+        for (int i = 0, cnt = 0; i < colors.length(); ++i) {
+            char cur = colors.charAt(i);
+            if (cur == prev) {
+                if (++cnt > 2) {
+                    if (cur == 'A') {
+                        ++a;
+                    } else {
+                        ++b;
+                    }
+                }
+            } else {
+                cnt = 1;
+            }
+            prev = cur;
+        }
+        return a > b;
+    }
+}

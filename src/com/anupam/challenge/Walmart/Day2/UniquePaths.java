288ef985b37ce571dba1e689725c98f637c0ce98
Akshat0410 <anupamp74@gmail.com>
2022-01-28 21:24:59 +0530

    4th question solved

---
+++ src/com/anupam/challenge/Walmart/Day2/UniquePaths.java
@@ -0,0 +1,24 @@
+package com.anupam.challenge.Walmart.Day2;
+
+//Question : - Number of Unique Paths
+
+import java.util.Scanner;
+
+public class UniquePaths {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt(),b=sc.nextInt();
+        int ans=NumberOfPath(a,b);
+        System.out.println(ans);
+    }
+
+    private static int NumberOfPath(int a, int b) {
+        int N = a+b-2;
+        int r= a-1;
+        double res = 1;
+        for(int i=1;i<=r;i++){
+            res = res * (N-r+i)/i;
+        }
+        return (int)res;
+    }
+}
